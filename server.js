d5d9099701041ec5239939e02b74a1594d913ffd
juliatbarbosa <103785929+juliatbarbosa@users.noreply.github.com>
2025-04-05 13:38:10 -0300

    CRUD MEDICAMENTOS COM NODE JS

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+require('./src/index')(app);
+app.use(express.static('public'));
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
